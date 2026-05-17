6ea86836e861e56402d89a289d841e9da723c747
Munna -Dev <mkcodes.in@gmail.com>
2026-05-17 11:11:02 +0530

    adding admin middleware

---
+++ backend/src/middlewares/role.middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+export const isAdmin = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const user = (req as any).user;
+        if (user.role !== "admin") {
+            return res.status(403).json({
+                success: false,
+                message: "Forbidden"
+            });
+        }
+        next();
+    };
+};
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { upload } from "../middlewares/upload";
 import { authMiddlware } from "../middlewares/auth.middleware";
 import { productController } from "../controllers/product.controller";
+import { isAdmin } from "../middlewares/role.middleware";
 
 export const productRouter = Router();
 
@@ -10,7 +11,7 @@ export const productRouter = Router();
  * @route POST /api/products/create
  * @access private (only admin)
  */
-productRouter.post("/create", upload.array("images", 5), authMiddlware, productController.addProduct);
+productRouter.post("/create", upload.array("images", 5), authMiddlware, isAdmin(), productController.addProduct);
 
 /**
  * @description Get all products
@@ -38,12 +39,12 @@ productRouter.get("/:id", productController.getProductById);
  * @route PUT /api/products/:id
  * @access public (only admin)
  */
-productRouter.patch("/:id", upload.array("images", 5), authMiddlware, productController.updateProduct);
+productRouter.patch("/:id", upload.array("images", 5), authMiddlware, isAdmin(), productController.updateProduct);
 
 /**
  * @description Delete a product
  * @route DELETE /api/products/:id
  * @access public (only admin)
  */
-productRouter.delete("/:id", authMiddlware, productController.deleteProduct);
+productRouter.delete("/:id", authMiddlware, isAdmin(), productController.deleteProduct);
 
