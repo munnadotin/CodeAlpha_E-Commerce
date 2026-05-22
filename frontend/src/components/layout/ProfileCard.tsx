9e327a09642c15cba102ef7901feb53bd2470511
Munna -Dev <mkcodes.in@gmail.com>
2026-05-22 16:19:39 +0530

    Access base control fixing

--- frontend/src/components/layout/ProfileCard.tsx
+++ frontend/src/components/layout/ProfileCard.tsx
@@ -1,4 +1,4 @@
-import { Handbag, LogOut, User2 } from "lucide-react";
+import { Cuboid, Handbag, LogOut, User2 } from "lucide-react";
 import type { User } from "../../types/auth.type";
 import { useDispatch } from "react-redux";
 import { clearUser } from "../../features/authSlice";
@@ -15,6 +15,31 @@ const ProfileCard = ({ activeTab, setActiveTab, user }: ActiveTabType) => {
     const navigate = useNavigate();
 
     if (!user) return null;
+
+    const profileAction = [
+        {
+            id: 1,
+            name: 'Profile',
+            role: ['user', 'admin'],
+            icon: <User2 strokeWidth={1.5} className="w-5 h-5" />,
+            onClick: () => setActiveTab('profile')
+        },
+        {
+            id: 2,
+            name: 'My Orders',
+            role: ['user'],
+            icon: <Handbag strokeWidth={1.5} className="w-5 h-5" />,
+            onClick: () => setActiveTab('orders')
+        },
+        {
+            id: 3,
+            name: 'Products',
+            role: ['admin'],
+            icon: <Cuboid strokeWidth={1.5} className="w-5 h-5" />,
+            onClick: () => setActiveTab('admin')
+        }
+    ];
+
 
     const handleLogout = () => {
         dispatch(clearUser())
@@ -33,26 +58,23 @@ const ProfileCard = ({ activeTab, setActiveTab, user }: ActiveTabType) => {
                     </div>
                     <h3 className="text-lg font-medium text-gray-900">{user.name}</h3>
                     <p className="text-sm text-gray-500 mt-1">{user.email}</p>
-                    {/* <p className="text-xs text-gray-400 mt-2">Member since {user.joinDate}</p> */}
                 </div>
 
                 {/* Navigation */}
                 <div className="p-4 space-y-1">
-                    <button
-                        onClick={() => setActiveTab('profile')}
-                        className={`w-full flex items-center gap-3 px-4 py-2.5 rounded-lg transition-all duration-200 ${activeTab === 'profile' ? 'bg-gray-900 text-white shadow-sm' : 'text-gray-700 hover:bg-gray-50'} cursor-pointer`}
-                    >
-                        <User2 strokeWidth={1.5} className="h-5 w-5" />
-                        <span className="text-sm font-medium">Profile</span>
-                    </button>
-
-                    <button
-                        onClick={() => setActiveTab('orders')}
-                        className={`w-full flex items-center gap-3 px-4 py-2.5 rounded-lg transition-all duration-200 ${activeTab === 'orders' ? 'bg-gray-900 text-white shadow-sm' : 'text-gray-700 hover:bg-gray-50'} cursor-pointer`}
-                    >
-                        <Handbag strokeWidth={1.5} className="h-5 w-5" />
-                        <span className="text-sm font-medium">My Orders</span>
-                    </button>
+                    {profileAction.map((action) => (
+                        <div key={action.id}>
+                            {action.role.includes(user.role) && (
+                                <button
+                                    onClick={() => setActiveTab(action.name.toLowerCase())}
+                                    className={`w-full flex items-center gap-3 px-4 py-2.5 rounded-lg transition-all duration-200 ${activeTab === action.name.toLowerCase() ? 'bg-gray-900 text-white shadow-sm' : 'text-gray-700 hover:bg-gray-50'} cursor-pointer`}
+                                >
+                                    {action.icon}
+                                    <span className="text-sm font-medium">{action.name}</span>
+                                </button>
+                            )}
+                        </div>
+                    ))}
 
                     <div className="pt-4 mt-4 border-t border-gray-100">
                         <button
@@ -65,7 +87,7 @@ const ProfileCard = ({ activeTab, setActiveTab, user }: ActiveTabType) => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- frontend/src/components/layout/ProfileContent.tsx
+++ frontend/src/components/layout/ProfileContent.tsx
@@ -31,58 +31,62 @@ function ProfileContent({ user }: ProfileContentProps) {
                     </div>
                 </div>
 
-                <div className="space-y-3">
-                    <label className="block text-sm font-medium text-gray-700 mb-2">
-                        Delivery Address
-                    </label>
+                {user.role === "user" && (
+                    <div className="space-y-3">
+                        <label className="block text-sm font-medium text-gray-700 mb-2">
+                            Delivery Address
+                        </label>
 
-                    {user.address && user.address.length > 0 ? (
-                        <div className="group relative space-y-3 grid grid-cols-1 md:grid-cols-2 gap-4">
-                            {user.address.map((address, index) => (
-                                <div
-                                    key={index}
-                                    className="rounded-xl flex items-start justify-between border border-gray-200 bg-white p-4 transition hover:shadow"
+                        {user.address && user.address.length > 0 ? (
+                            <div className="space-y-3 grid grid-cols-1 md:grid-cols-2 gap-4">
+                                {user.address.map((address, index) => (
+                                    <div
+                                        key={index}
+                                        className="rounded-xl flex items-start justify-between border border-gray-200 bg-white p-4 transition"
+                                    >
+                                        <div className="flex flex-col gap-2">
+                                            <p className="text-sm font-medium text-gray-900">
+                                                {address.street}
+                                            </p>
+
+                                            <p className="mt-1 text-sm text-gray-600">
+                                                {address.city}, {address.state} - {address.zipCode}
+                                            </p>
+
+                                            <p className="text-sm text-gray-500">
+                                                {address.country}
+                                            </p>
+                                        </div>
+                                        <div className="flex items-center gap-3">
+                                            <button className="p-1.5 bg-red-500 text-white rounded-md hover:bg-red-600 transition cursor-pointer">
+                                                <Trash2 strokeWidth={1.5} className="w-4 h-4" />
+                                            </button>
+                                            <button className="p-1.5 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
+                                                <SquarePen strokeWidth={1.5} className="w-4 h-4" />
+                                            </button>
+                                        </div>
+
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <div className="flex flex-col items-center justify-center rounded-xl border border-dashed border-gray-300 bg-gray-50 py-10">
+                                <p className="text-sm text-gray-500">
+                                    No delivery address added yet
+                                </p>
+
+                                <button
+                                    onClick={() => setActiveModel(true)}
+                                    type="button"
+                                    className="mt-4 rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-800 cursor-pointer"
                                 >
-                                    <div className="flex flex-col gap-2">
-                                        <p className="text-sm font-medium text-gray-900">
-                                            {address.street}
-                                        </p>
-
-                                        <p className="mt-1 text-sm text-gray-600">
-                                            {address.city}, {address.state} - {address.zipCode}
-                                        </p>
-
-                                        <p className="text-sm text-gray-500">
-                                            {address.country}
-                                        </p>
-                                    </div>
-                                    <div className="flex items-center gap-3">
-                                        <button className="p-1.5 bg-red-500 text-white rounded-md hover:bg-red-600 transition cursor-pointer">
-                                            <Trash2 strokeWidth={1.5} className="w-4 h-4" />
-                                        </button>
-                                        <button className="p-1.5 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
-                                            <SquarePen strokeWidth={1.5} className="w-4 h-4" />
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <div className="flex flex-col items-center justify-center rounded-xl border border-dashed border-gray-300 bg-gray-50 py-10">
-                            <p className="text-sm text-gray-500">
-                                No delivery address added yet
-                            </p>
-
-                            <button
-                                onClick={() => setActiveModel(true)}
-                                type="button"
-                                className="mt-4 rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white transition hover:bg-gray-800 cursor-pointer"
-                            >
-                                Add Address
-                            </button>
-                        </div>
-                    )}
-                </div>
+                                    Add Address
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                )
+                }
 
                 {/* popup model */}
                 {activeModel && (<AddressModel setActiveModel={setActiveModel} />)}
