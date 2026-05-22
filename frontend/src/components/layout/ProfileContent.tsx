a09b3317ff7a2ff949bd4621b55a1f1f58b3e57c
Munna -Dev <mkcodes.in@gmail.com>
2026-05-22 11:15:20 +0530

    address fixing

--- frontend/src/components/layout/ProfileContent.tsx
+++ frontend/src/components/layout/ProfileContent.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { User } from "../../types/auth.type"
 import AddressModel from "./AddressModel";
+import { SquarePen, Trash2 } from "lucide-react";
 
 interface ProfileContentProps {
     user: User
@@ -36,23 +37,33 @@ function ProfileContent({ user }: ProfileContentProps) {
                     </label>
 
                     {user.address && user.address.length > 0 ? (
-                        <div className="space-y-3">
+                        <div className="group relative space-y-3 grid grid-cols-1 md:grid-cols-2 gap-4">
                             {user.address.map((address, index) => (
                                 <div
                                     key={index}
-                                    className="rounded-xl border border-gray-200 bg-white p-4 transition hover:shadow"
+                                    className="rounded-xl flex items-start justify-between border border-gray-200 bg-white p-4 transition hover:shadow"
                                 >
-                                    <p className="text-sm font-medium text-gray-900">
-                                        {address.street}
-                                    </p>
+                                    <div className="flex flex-col gap-2">
+                                        <p className="text-sm font-medium text-gray-900">
+                                            {address.street}
+                                        </p>
 
-                                    <p className="mt-1 text-sm text-gray-600">
-                                        {address.city}, {address.state} - {address.zipCode}
-                                    </p>
+                                        <p className="mt-1 text-sm text-gray-600">
+                                            {address.city}, {address.state} - {address.zipCode}
+                                        </p>
 
-                                    <p className="text-sm text-gray-500">
-                                        {address.country}
-                                    </p>
+                                        <p className="text-sm text-gray-500">
+                                            {address.country}
+                                        </p>
+                                    </div>
+                                    <div className="flex items-center gap-3">
+                                        <button className="p-1.5 bg-red-500 text-white rounded-md hover:bg-red-600 transition cursor-pointer">
+                                            <Trash2 strokeWidth={1.5} className="w-4 h-4" />
+                                        </button>
+                                        <button className="p-1.5 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition cursor-pointer">
+                                            <SquarePen strokeWidth={1.5} className="w-4 h-4" />
+                                        </button>
+                                    </div>
                                 </div>
                             ))}
                         </div>
